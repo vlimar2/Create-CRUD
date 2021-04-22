dd95c5d7dade6708c0f8c277c47346df35230628
Rafael Simião de Lima <raphael956@hotmail.com>
2021-04-22 12:02:29 -0300

    First commit

---
+++ front/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FuncAddComponent } from './func-add/func-add.component';
+import { FuncEditComponent } from './func-edit/func-edit.component';
+import { FuncGetComponent } from './func-get/func-get.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FuncAddComponent,
+    FuncEditComponent,
+    FuncGetComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SlimLoadingBarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
